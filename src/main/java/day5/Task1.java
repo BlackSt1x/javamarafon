581caa4592dec70623df478d10ad9405ab5bdd23
Slava <slavablackjack@gmail.com>
2021-10-15 21:27:40 +0200

    add 5 day

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -11,4 +11,32 @@ public class Task1 {
 
     }
 }
+class Car {
 
+    private String  model;
+    private String color;
+    private int year;
+
+
+    public void SetModel(String model){
+        this.model = model;
+    }
+    public String GetModel(){
+        return model;
+    }
+    public void SetColor(String color){
+        this.color = color;
+    }
+    public String GetColor(){
+        return color;
+    }
+    public void  SetYear(int year){
+        this.year = year;
+    }
+    public int GetYear() {
+        return year;
+    }
+    public void setInfo(){
+        System.out.println("BEST CAR IN WORLD " +GetModel() + " in " +GetColor() + " color " + "was born " +GetYear()+ " year !!!");
+    }
+}
