d0811b015450ca37b799e587e0f3eb2903762f76
Slava <slavablackjack@gmail.com>
2021-10-15 21:30:34 +0200

    add 5 day

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,36 +1,9 @@
 package day5;
 
-
-import java.sql.ShardingKey;
-
 public class Task2 {
     public static void main(String[] args) {
-        Motorbike Harley = new Motorbike();
+        Motorbike Harley = new Motorbike(" \"Harley\"", "White", 1901);
+        System.out.println("Nice bike model "+ Harley.GetModel() + " in "+ Harley.GetColor() + " color, was born in " + Harley.GetYear() + " year !!! Sanx for this we say from 2 best guys William Harley and Arthur Davidson!");
 
-    }
-}
-class Motorbike {
-    private String model;
-    private String color;
-    private int year;
-
-    public Motorbike() {
-        this.model = "\"Harley\"";//Прочитал все методы в классе String  и не  нашел... среда разработки помогла//
-        this.color = "White";
-        this.year = 1901;
-        System.out.println("Nice bike model "+GetModel() + " in "+ GetColor() + " color, was born in " + GetYear() + " year !!! Sanx for this we say from 2 best guys William Harley and Arthur Davidson!");
-    }
-
-
-    public String GetModel() {
-        return model;
-    }
-
-    public String GetColor() {
-        return color;
-    }
-
-    public int GetYear() {
-        return year;
     }
 }
